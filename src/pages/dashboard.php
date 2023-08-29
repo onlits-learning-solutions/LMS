f0a62c0cdd8e9b7ace647c098b19702546aa362b
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2023-08-29 10:18:48 +0530

    Start grid development Fixes #12

---
+++ src/pages/dashboard.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LMS - Dashboard</title>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="topnav">
+            <?php require 'navbar.php' ?>
+        </div>
+        <div class="sub-container">
+            <aside class="sidenav">
+                <?php require 'sidebar.php' ?>
+            </aside>
+            <main class="main">
+                <div class="content">
+                    <!-- You content goes here!  -->
+                    <h1>Dashboard</h1> <br>
+                    <div class="card-container">
+                        <div class="card">
+                            <div class="card-title">
+                                <p>Books</p>
+                            </div>
+                            <div class="card-body">
+                                <p>10</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-title">
+                                <p>Books</p>
+                            </div>
+                            <div class="card-body">
+                                <p>10</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-title">
+                                <p>Books</p>
+                            </div>
+                            <div class="card-body">
+                                <p>10</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-title">
+                                <p>Books</p>
+                            </div>
+                            <div class="card-body">
+                                <p>10</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-title">
+                                <p>Books</p>
+                            </div>
+                            <div class="card-body">
+                                <p>10</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-title">
+                                <p>Books</p>
+                            </div>
+                            <div class="card-body">
+                                <p>10</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-title">
+                                <p>Books</p>
+                            </div>
+                            <div class="card-body">
+                                <p>10</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <footer class="footer">
+                    <?php require 'footer.php' ?>
+                </footer>
+            </main>
+        </div>
+    </div>
+</body>
+
+</html>
--- src/pages/logout.php
+++ src/pages/logout.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
-unset($_SESSION['user_id']); 
-header("location: index.php");
+session_destroy(); 
+header("location:../index.php");
 ?>
